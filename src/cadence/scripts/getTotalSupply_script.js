73665b172560126a762feb19e2cdce4bee5008fe
ankurdaharwal <ankur.daharwal@gmail.com>
2022-06-26 07:02:21 +0530

    🎉 initial commit

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,11 +1,11 @@
 export const getTotalSupply =
-// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-`
-import BottomShot from 0x7b6adb682517f137;
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import Vikings from 0xc1cdddc38fbc6a4a;
 
 pub fun main(): UInt64 {
 
     return BottomShot.totalSupply;
 
 }
-`
+`;
